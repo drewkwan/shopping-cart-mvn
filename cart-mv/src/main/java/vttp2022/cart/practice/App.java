2fe9230f2453479055bb013ab41f8d5e3837019e
drewkwan <andrewtimkwan@gmail.com>
2022-06-09 22:19:24 +0800

    initial commit

---
+++ cart-mv/src/main/java/vttp2022/cart/practice/App.java
@@ -0,0 +1,15 @@
+package vttp2022.cart.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome to your shopping cart!");
+        List<String> cart = new LinkedList<>();
+        Console cons = System.console();
+
+}
