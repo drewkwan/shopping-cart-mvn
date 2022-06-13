dc4ad8b1fa29f4f620ab7a20af258298a230ef7a
drewkwan <andrewtimkwan@gmail.com>
2022-06-13 15:40:56 +0800

    Added the add function

--- cart-mv/src/main/java/vttp2022/cart/practice/App.java
+++ cart-mv/src/main/java/vttp2022/cart/practice/App.java
@@ -1,5 +1,9 @@
 package vttp2022.cart.practice;
 
+import java.io.Console;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,8 +12,59 @@ public class App
 {
     public static void main( String[] args )
     {
+        //define objects - Linked list, console, input command, delIndex, stop
         System.out.println("Welcome to your shopping cart!");
         List<String> cart = new LinkedList<>();
         Console cons = System.console();
+        String input;
+        int delIndex;
+        boolean stop = false;
+
+        //pre-add items to cart for testing
+        cart.add("apple");
+        cart.add("pear");
+        cart.add("orange");
+
+        //main loop
+        while (!stop) {
+            //indicate console command 
+            input = cons.readLine("> ");
+            System.out.printf("READ: %s\n", input);
+            String[] terms = input.split(" ");
+            String cmd = terms[0];
+
+            switch(cmd.toLowerCase()) {
+                case "add":
+                    //ensure readability of the typed commands
+                    String fruitsStr = terms[1]; 
+                    String fruitsReplaced = fruitsStr.replace(",", " ") //replace the commas with a space which gets split(?) try a trim?
+                    String[] fruits = fruitsReplaced.split(" ");
+
+                    //the command cannot be empty
+                    for (i = 0; i < fruits.length; i++) {
+                        boolean found = false;
+                        //cart cannot be empty?
+                        for (int j = 0, j < cart.size(), j++) { //use size not length for lists
+                            if (fruits.toUpperCase().equals(cart.get(j).toUpperCase()))
+                            System.out.printf("That item is already in your cart!");
+                            found = true;// what's this for?
+
+                            break;
+                        }
+
+                        if (!found) {
+                            cart.add(fruits[i]);
+                            System.out.printf("%s has been added to your cart successfully!", fruits[i]); //print receipt
+                        } 
+
+                    }
+
+                break; //write your break after your case first if you're going to need it. Track your lil brackets. 
+
+            }
+        }
+
+
+
 
 }
