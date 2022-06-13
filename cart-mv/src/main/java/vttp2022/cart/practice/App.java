1619ea57a6e7f3ec257ca9c369e67f1744c0e694
drewkwan <andrewtimkwan@gmail.com>
2022-06-13 16:00:09 +0800

    added  list function

--- cart-mv/src/main/java/vttp2022/cart/practice/App.java
+++ cart-mv/src/main/java/vttp2022/cart/practice/App.java
@@ -37,15 +37,15 @@ public class App
                 case "add":
                     //ensure readability of the typed commands
                     String fruitsStr = terms[1]; 
-                    String fruitsReplaced = fruitsStr.replace(",", " ") //replace the commas with a space which gets split(?) try a trim?
+                    String fruitsReplaced = fruitsStr.replace(",", " "); //replace the commas with a space which gets split(?) try a trim?
                     String[] fruits = fruitsReplaced.split(" ");
 
                     //the command cannot be empty
-                    for (i = 0; i < fruits.length; i++) {
+                    for (int i = 0; i < fruits.length; i++) {
                         boolean found = false;
                         //cart cannot be empty?
-                        for (int j = 0, j < cart.size(), j++) { //use size not length for lists
-                            if (fruits.toUpperCase().equals(cart.get(j).toUpperCase()))
+                        for (int j = 0; j < cart.size(); j++) { //use size not length for lists
+                            if (fruits[i].toUpperCase().equals(cart.get(j).toUpperCase()))
                             System.out.printf("That item is already in your cart!");
                             found = true;// what's this for?
 
@@ -59,12 +59,25 @@ public class App
 
                     }
 
-                break; //write your break after your case first if you're going to need it. Track your lil brackets. 
+                    break; //write your break after your case first if you're going to need it. Track your lil brackets. 
 
+                case "list":
+                    /* In list, we want to display what is in the cart, in the right numbered format. If the cart is empty, return that the cart is empty. 
+                    Let's start with if empty */
+                    if (cart.size() <= 0) {
+                        System.out.printf("Your cart is empty.");
+                    }
+                    if (cart.size()> 0) {
+                        for (int i = 0; i < cart.size(); i++) { //what is this i loop for?
+                            System.out.printf("%d. %s\n", i+1, cart.get(i));    
+                        }
+                    }
+                    break;
+
+                
             }
+        
         }
 
-
-
-
+    }
 }
